380628c59833ed08b13abfe8979800fbd6ef5ff5
Ryan Kabir <rkabir@alum.mit.edu>
2011-11-22 17:51:03 -0400

    Initial Commit

---
+++ og3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  og3
+//
+//  Created by Ryan Kabir on 11/22/11.
+//  Copyright (c) 2011 Grow20 Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ og3/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  og3
+//
+//  Created by Ryan Kabir on 11/22/11.
+//  Copyright (c) 2011 Grow20 Corporation. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
