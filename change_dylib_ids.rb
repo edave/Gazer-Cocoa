da6368f8b3baf9e41a71ad186188cf1358a6cd90
Ryan Kabir <rkabir@alum.mit.edu>
2011-11-29 17:33:42 -0400

    jimmy is dead, long live a real name

---
+++ change_dylib_ids.rb
@@ -0,0 +1,13 @@
+dylibs = Dir.glob("*.dylib")
+dylibs.each do |path|
+  name = path.split("/").last
+  x = %x[otool -L #{path}]
+  lines = x.split("\n")
+  lines = lines.map(&:strip)
+  op_lines = lines.select{|l| l.include?("opt")}.map{|m| m.split.first}
+  %x[install_name_tool -id @executable_path/../Frameworks/#{name} #{path}]
+  # op_lines.each do |line|
+  #   lib = line.split("/").last
+  #   %x[install_name_tool -change #{line} @executable_path/../Frameworks/#{lib} #{path}]
+  # end
+end
