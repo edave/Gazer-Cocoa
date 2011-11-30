a31d8da780c0c0dc898aab3762dc850bd6cc85bb
Ryan Kabir <rkabir@alum.mit.edu>
2011-11-30 00:30:37 -0400

    face detection capability

--- og3/main.mm
+++ og3/main.mm
@@ -12,7 +12,14 @@
 #include "MainGazeTracker.h"
 #include "WindowStore.h"
 
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+#include <stdio.h>
+
 #import <Cocoa/Cocoa.h>
+#include "CoreFoundation/CoreFoundation.h"
 
 //int main(int argc, char *argv[])
 //{
@@ -20,6 +27,9 @@
 //}
 
 #define MAIN_WINDOW_NAME "OpenGazer"
+
+using namespace std;
+using namespace cv;
 
 MainGazeTracker* gazeTracker;
 
@@ -105,16 +115,72 @@ void registerMouseCallbacks() {
     cvSetMouseCallback(MAIN_WINDOW_NAME, mouseClick, NULL);
 }
 
+String face_cascade_name = "haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+string window_name = "Capture - Face detection";
+
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+RNG rng(12345);
+
+
 void drawFrame() {
     cvShowImage(MAIN_WINDOW_NAME, gazeTracker->canvas.get());
+    Mat frame = gazeTracker->videoinput->frame;
+    if( !frame.empty() ) {
+        std::vector<cv::Rect> faces;
+        cv::Mat frame_gray;
+        
+        cvtColor(frame, frame_gray, CV_BGR2GRAY);
+        equalizeHist( frame_gray, frame_gray );
+
+        face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(30, 30) );
+        
+        for( int i = 0; i < faces.size(); i++ )
+        {
+            cv::Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+            ellipse( frame, center, cv::Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+            
+            cv::Mat faceROI = frame_gray( faces[i] );
+            std::vector<cv::Rect> eyes;
+            
+            //-- In each face, detect eyes
+            eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, cv::Size(30, 30) );
+            
+            for( int j = 0; j < eyes.size(); j++ )
+            {
+                cv::Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
+                int radius = cvRound( (eyes[j].width + eyes[i].height)*0.25 );
+                circle( frame, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+            }
+        }
+        // just for debug
+        //imshow( window_name, frame );
+        
+    }
 }
 
 int main(int argc, char **argv) {
+    // set the right path so the classifiers can find their data
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
+    char path[PATH_MAX];
+    if (!CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX))
+    {
+        // error!
+    }
+    CFRelease(resourcesURL);    
+    chdir(path);
+
     gazeTracker = new MainGazeTracker(argc, argv, getStores());
 
     cvNamedWindow(MAIN_WINDOW_NAME, CV_GUI_EXPANDED);
     cvResizeWindow(MAIN_WINDOW_NAME, 640, 480);
 
+    system("pwd");
+    if( !face_cascade.load( face_cascade_name ) ){ printf("\n\n--(!)Error loading face\n"); return -1; };
+    if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("\n\n--(!)Error loading eyes\n"); return -1; };
+    
 //    createButtons();
     registerMouseCallbacks();
 
