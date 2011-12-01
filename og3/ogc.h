6e84c21f96a42aea2ab55044958a4414077b9d8c
Ryan Kabir <rkabir@alum.mit.edu>
2011-12-01 15:43:49 -0400

    ogc classes - DANGER ZONE

---
+++ og3/ogc.h
@@ -0,0 +1,14 @@
+//
+//  ogc.h
+//  og3
+//
+//  Created by Ryan Kabir on 12/1/11.
+//  Copyright (c) 2011 Grow20 Corporation. All rights reserved.
+//
+
+#ifndef og3_ogc_h
+#define og3_ogc_h
+
+
+
+#endif
---
+++ og3/ogc.mm
@@ -0,0 +1,9 @@
+//
+//  ogc.mm
+//  og3
+//
+//  Created by Ryan Kabir on 12/1/11.
+//  Copyright (c) 2011 Grow20 Corporation. All rights reserved.
+//
+
+#include <iostream>
